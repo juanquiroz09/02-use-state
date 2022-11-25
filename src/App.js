78476e643cf57442f6330de935c093f567bd8754
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-25 12:17:21 -0500

    ejercicio1-terminado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import MiPrimerEstado from './components/MiPrimerEstado';
+import EjercicioComponent from './components/EjercicioComponent';
 
 function App() {
+  const fecha = new Date();
+  const yearActual = fecha.getFullYear();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>El estado en React - Hook useState</h1>
         <MiPrimerEstado></MiPrimerEstado>
+        <EjercicioComponent year={yearActual}></EjercicioComponent>
       </header>
     </div>
   );
